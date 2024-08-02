ad1140b8201b0036e9dfd7bf71c7d2fd4ac7bb13
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-08-02 12:00:39 -0400

    Object destructuring example

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 const my_math = require("./my_math");
 const person = require("./person");
 
+function calculatePersonAge({birthYear})
+{
+	// Person object destructured
+	return new Date().getFullYear() - birthYear;
+}
+
 function printGcd(a, b)
 {
 	console.log("gcd(" + a + ", " + b + ") = " + my_math.gcd(a, b));
@@ -14,4 +20,5 @@ printGcd(72, -108);
 console.log("\nBig Shaq: " + my_math.skrrra() + "\n");
 
 gustav = person.makePerson("Gustav", "Romeo", 1996);
-console.log(gustav.toString() + "\n");
+console.log(gustav.toString());
+console.log("Age: " + calculatePersonAge(gustav) + "\n");
